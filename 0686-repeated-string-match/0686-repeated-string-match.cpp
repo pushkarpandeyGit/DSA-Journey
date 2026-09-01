8b8959c6fb02a5869462181d57be56dbb8fd7cd7
pushkarpandeyGit <pushkarpandey2162@gmail.com>
2026-09-01 20:33:47 +0530

    Time: 6 ms (50.17%), Space: 9.7 MB (65.77%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string temp=a;
+        int count=1;
+        while(a.size()<b.size()){
+            a+=temp;
+            count++;
+        }
+        if(a.find(b) != string::npos) return count;
+        a+=temp;
+        if(a.find(b) != string::npos) return count+1;
+        return -1;
+    }
+};
