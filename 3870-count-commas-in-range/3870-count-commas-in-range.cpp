2196fcd839dfbced1d7d7dae5a3844c7761cf17d
pushkarpandeyGit <pushkarpandey2162@gmail.com>
2026-09-08 10:17:40 +0530

    Time: 0 ms (100%), Space: 9.2 MB (5.52%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     int countCommas(int n) {
         if(to_string(n).size()<=3) return 0;
-        int c=0;
-        for(int i=1000;i<=n;i++){
-            c++;
-        }
-        return c;
+        return n-999;
     }
 };
