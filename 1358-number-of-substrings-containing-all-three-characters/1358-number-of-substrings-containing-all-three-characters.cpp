aff108c76d3dadea826343e779c8041880beeb45
pushkarpandeyGit <pushkarpandey2162@gmail.com>
2026-09-11 20:59:25 +0530

    Time: 47 ms (5.37%), Space: 16.9 MB (8.1%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -5,11 +5,11 @@ public:
         for(int i=0;i<s.size();i++){
             m[s[i]]++;
             while(m.size()==3){
+                c += s.size() - i;
                 m[s[l]]--;
                 if(m[s[l]] == 0) m.erase(s[l]);
                 l++;
             }
-            c+=l;
         }
         return c;
     }
