281912b3ccf6d473d3cac70b094ec74c9a2d4a07
pushkarpandeyGit <pushkarpandey2162@gmail.com>
2026-09-17 14:52:06 +0530

    Time: 11 ms (94.6%), Space: 87.7 MB (14.33%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int ans=0;stack<int> s;
-        for(int i=0;i<heights.size();i++){
-            while(!s.empty() && heights[s.top()]>heights[i]){
-                int h=heights[s.top()];s.pop();
-                int width;
-                if(s.empty()) width=i;
-                else width=i-s.top()-1;
-                int area=h*width;
-                ans=max(ans,area);
+    void nse (vector<int> &nums,stack<int> & s,vector<int> &ans){
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && nums[s.top()]>=nums[i]){
+                s.pop();
             }
+            if(s.empty()) ans[i]=n-i;
+            else ans[i]=s.top()-i;
             s.push(i);
         }
-        while(!s.empty()){
-            int h=heights[s.top()]; s.pop();
-            int width;
-            if(s.empty())
-                width=heights.size();
-            else
-                width=heights.size()-s.top()-1;
-            int area=h*width;
-            ans=max(ans,area);
+    }
+    void pse (vector<int> &nums,stack<int> & s,vector<int>& ans){
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            while(!s.empty() && nums[s.top()] > nums[i]){
+                s.pop();
+            }
+            if(s.empty()) ans[i]=i+1;
+           else ans[i]=i-s.top();
+            s.push(i);
+        }
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size(); int ans=0;stack<int> s; vector<int> nextsmall(n,0); vector<int> presmall(n,0);
+
+        nse(heights,s,nextsmall);
+        while(!s.empty()) s.pop();
+        pse(heights,s,presmall);
+
+        for(int i=0;i<n;i++){
+            ans=max(ans,heights[i]*(nextsmall[i]+presmall[i]-1));
         }
         return ans;
     }
